ab0662627f9d2f921ff28783d0fd816cb75143f2
Rijul Gupta <rijulg@gmail.com>
2024-02-06 12:58:42 +0000

    chore: add some more test

--- benchmark/test/database_generator/test_imbalanced_data.py
+++ benchmark/test/database_generator/test_imbalanced_data.py
@@ -19,7 +19,7 @@ database_generator = DatabaseGenerator(
 )
 
 
-def test():
+def test_1():
     db_name = str(uuid4())
     diagnosis_keys = ["organic"] * 10 + ["muscle_tension"] * 3
     age = None
@@ -43,9 +43,91 @@ def test():
         (dataset.train_sessions, "muscle_tension", 1),
         (dataset.test_sessions, "muscle_tension", 1),
         (dataset.val_sessions, "muscle_tension", 1),
-        (dataset.train_sessions, "organic", 8),
-        (dataset.test_sessions, "organic", 1),
-        (dataset.val_sessions, "organic", 1),
+        (dataset.train_sessions, "organic", 8),  # 7
+        (dataset.test_sessions, "organic", 1),  # 2
+        (dataset.val_sessions, "organic", 1),  # 1
+    ]
+    for bucket, diagnosis_key, expected_count in expected_organic:
+        assert expected_count == count_sessions(
+            sessions=bucket,
+            diagnosis_key=diagnosis_key,
+            gender=gender,
+            age_range=age,
+        )
+
+
+def test_2():
+    db_name = str(uuid4())
+    diagnosis_keys = ["organic"] * 100 + ["muscle_tension"] * 30
+    age = None
+    gender = ""
+    sessions = [
+        ProcessedSession(
+            id=str(uuid4()),
+            age=age,
+            gender=gender,
+            diagnosis=[diagnosis_map.get(diagnosis_key)],
+            files=[],
+        )
+        for diagnosis_key in diagnosis_keys
+    ]
+    dataset = database_generator.generate(
+        db_name=db_name,
+        sessions=sessions,
+    )
+    assert_all_sessions_allocated(sessions, dataset)
+    expected_organic = [
+        (dataset.train_sessions, "muscle_tension", 12),  # 21
+        (dataset.test_sessions, "muscle_tension", 12),  # 6
+        (dataset.val_sessions, "muscle_tension", 6),  # 3
+        (dataset.train_sessions, "organic", 79),  # 70
+        (dataset.test_sessions, "organic", 14),  # 20
+        (dataset.val_sessions, "organic", 7),  # 10
+    ]
+    for bucket, diagnosis_key, expected_count in expected_organic:
+        assert expected_count == count_sessions(
+            sessions=bucket,
+            diagnosis_key=diagnosis_key,
+            gender=gender,
+            age_range=age,
+        )
+
+
+def test_3():
+    """
+    seems balanced because we have 10 of each organic, muscle_tension and healthy
+    but since organic and muscle_tension fall under pathological and healthy is its own class
+    it is imbalaced with 20 for pathological and 10 for healthy.
+    """
+    db_name = str(uuid4())
+    diagnosis_keys = ["organic"] * 10 + ["muscle_tension"] * 10 + ["healthy"] * 10
+    age = None
+    gender = ""
+    sessions = [
+        ProcessedSession(
+            id=str(uuid4()),
+            age=age,
+            gender=gender,
+            diagnosis=[diagnosis_map.get(diagnosis_key)],
+            files=[],
+        )
+        for diagnosis_key in diagnosis_keys
+    ]
+    dataset = database_generator.generate(
+        db_name=db_name,
+        sessions=sessions,
+    )
+    assert_all_sessions_allocated(sessions, dataset)
+    expected_organic = [
+        (dataset.train_sessions, "muscle_tension", 7),  # 7
+        (dataset.test_sessions, "muscle_tension", 2),  # 2
+        (dataset.val_sessions, "muscle_tension", 1),  # 1
+        (dataset.train_sessions, "organic", 8),  # 7
+        (dataset.test_sessions, "organic", 1),  # 2
+        (dataset.val_sessions, "organic", 1),  # 1
+        (dataset.train_sessions, "healthy", 6),  # 7
+        (dataset.test_sessions, "healthy", 3),  # 2
+        (dataset.val_sessions, "healthy", 1),  # 1
     ]
     for bucket, diagnosis_key, expected_count in expected_organic:
         assert expected_count == count_sessions(
