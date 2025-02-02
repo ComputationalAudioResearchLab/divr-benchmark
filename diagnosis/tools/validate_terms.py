a87e6a4c5d4aba64e18d577fce50f39b0f5981fc
Rijul Gupta <rijulg@gmail.com>
2025-02-02 04:41:47 +0000

    chore: add aliases

--- diagnosis/tools/validate_terms.py
+++ diagnosis/tools/validate_terms.py
@@ -14,10 +14,13 @@ class AliasDict:
     def __init__(self, diagnosis_map: DiagnosisMap) -> None:
         with open(self.__alias_file, "r") as alias_file:
             alias_terms = yaml.full_load(alias_file)
+        self.__all_terms = {}
         for aliases in alias_terms:
             for alias in aliases:
+                self.__all_terms[alias] = aliases
                 if alias in diagnosis_map:
-                    self.__index_forward[alias] = aliases
+                    alias_key = diagnosis_map[alias].name
+                    self.__index_forward[alias_key] = aliases
         for key, aliases in self.__index_forward.items():
             for alias in aliases:
                 self.__index_reverse[alias] = key
@@ -28,6 +31,10 @@ class AliasDict:
     def __contains__(self, term: str) -> bool:
         return term in self.__index_reverse
 
+    @property
+    def all_terms(self) -> dict[str, list[str]]:
+        return self.__all_terms
+
 
 class ValidateTermsOthers:
     __curdir = Path(__file__).parent.resolve()
@@ -37,22 +44,40 @@ class ValidateTermsOthers:
         self.__diagnosis_map = diagnosis_map
         self.__alias = AliasDict(diagnosis_map)
         with open(self.__terms_file, "r") as terms_file:
-            self.__terms = yaml.full_load(terms_file)
+            terms = yaml.full_load(terms_file)
+            self.__terms = self.__coalesce_by_alias(terms)
 
     def run(self):
         present_terms = []
         absent_terms = {}
         terms_to_alias = {}
-        for term, dbs in self.__terms.items():
+        for term, aliases in self.__terms.items():
             if term in self.__diagnosis_map:
                 present_terms += [term]
             elif term in self.__alias:
-                terms_to_alias[term] = self.__alias[term]
+                alias_key = self.__alias[term]
+                terms_to_alias[term] = alias_key
+                for alias in aliases:
+                    terms_to_alias[alias] = alias_key
             else:
-                absent_terms[term] = dbs
+                absent_terms[term] = aliases
         print(f"{len(present_terms)} Terms found: ", present_terms)
         print(f"{len(terms_to_alias)} Terms to alias: ", terms_to_alias)
         print(f"{len(absent_terms)} Terms absent: ", absent_terms.keys())
+
+    def __coalesce_by_alias(self, terms: dict[str, list[str]]) -> dict[str, list[str]]:
+        new_terms = {}
+        old_terms = list(terms.keys())
+        while len(old_terms) > 0:
+            term = old_terms.pop(0)
+            new_terms[term] = []
+            if term in self.__alias.all_terms:
+                for alias in self.__alias.all_terms[term]:
+                    if alias in old_terms:
+                        new_terms[term] += [alias]
+                        old_terms.remove(alias)
+
+        return new_terms
 
 
 class ValidateTermsUSVAC:
